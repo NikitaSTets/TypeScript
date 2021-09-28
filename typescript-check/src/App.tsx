4c2b97f7767e2e326e8c4c0693d31d9c17503042
Nikita Stets <Nikita.stets.999@mail.ru>
2021-09-28 08:41:18 +0300

    practical assign part 1

--- typescript-check/src/App.tsx
+++ typescript-check/src/App.tsx
@@ -2,6 +2,78 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//DECORATOR
+function enumerable(value: boolean) {
+  return function (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor) {
+        
+        console.log('a')
+  };
+}
+
+//MIXIN
+type Constructor<T = {}> = new (...args: any[]) => T;
+
+// A mixin that adds a property
+function Timestamped<TBase extends Constructor>(Base: TBase) {
+  return class extends Base {
+    timestamp = Date.now();
+  };
+}
+
+// a mixin that adds a property and methods
+function Activatable<TBase extends Constructor>(Base: TBase) {
+  return class extends Base {
+    isActivated = false;
+
+    @enumerable(false)
+    activate() {
+      this.isActivated = true;
+    }
+
+    deactivate() {
+      this.isActivated = false;
+    }
+  };
+}
+
+class User {
+  name = '';
+}
+
+const TimestampedUser = Timestamped(User);
+
+const TimestampedActivatableUser = Timestamped(Activatable(User));
+
+const timestampedUserExample = new TimestampedUser();
+console.log(timestampedUserExample.timestamp);
+
+const timestampedActivatableUserExample = new TimestampedActivatableUser();
+timestampedActivatableUserExample.activate();
+
+console.log(timestampedActivatableUserExample.timestamp);
+console.log(timestampedActivatableUserExample.isActivated);
+
+
+
+
+type Getters<Type> = {
+    [Property in keyof Type as `get${Capitalize<string & Property>}`]: () => Type[Property]
+};
+ 
+interface Person {
+    name: string;
+    age: number;
+    location: string;
+}
+ 
+type LazyPerson = Getters<Person>;
+ 
+class a implements Person
+
+
+console.log(a);
+
+
 function App() {
   return (
     <div className="App">
